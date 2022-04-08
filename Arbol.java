3f014252405fd9b5a10828ca2c6ac2811f36d97c
DanielMorales1103 <mor21785@uvg.edu.gt>
2022-04-08 11:59:20 -0600

    Create Arbol.java
    Creación de Arbol

---
+++ Arbol.java
@@ -0,0 +1,70 @@
+/*
+ * Implementado de: 
+ * 	2020 Luis Cabrera Benito
+ *  Licenciado bajo la licencia MIT
+ *  https://parzibyte.me/blog/2020/05/08/arbol-binario-java/
+ */
+public class Arbol {
+	private Nodo raiz;
+
+    public Arbol() {
+
+    }
+    public boolean existe(String busqueda) {
+        return existe(this.raiz, busqueda);
+    }
+
+    private boolean existe(Nodo n, String busqueda) {
+        if (n == null) {
+            return false;
+        }
+        String[] cambio = n.getDato().split(",");        
+        if (cambio[0].trim().equals(busqueda.toLowerCase())) {
+            return true;
+        } else if (busqueda.toLowerCase().compareTo(cambio[0].trim()) < 0) {
+            return existe(n.getIzquierda(), busqueda);
+        } else {
+            return existe(n.getDerecha(), busqueda);
+        }
+
+    }
+    
+    public void insertar(String dato) {
+        if (this.raiz == null) {
+            this.raiz = new Nodo(dato);
+        } else {
+            this.insertar(this.raiz, dato);
+        }
+    }
+    
+    private void insertar(Nodo padre, String dato) {
+        if (dato.compareTo(padre.getDato()) > 0) {
+            if (padre.getDerecha() == null) {
+                padre.setDerecha(new Nodo(dato));
+            } else {
+                this.insertar(padre.getDerecha(), dato);
+            }
+        } else {
+            if (padre.getIzquierda() == null) {
+                padre.setIzquierda(new Nodo(dato));
+            } else {
+                this.insertar(padre.getIzquierda(), dato);
+            }
+        }
+    }
+    
+    private void inorden(Nodo n) {
+        if (n != null) {
+            inorden(n.getIzquierda());
+            n.imprimirDato();
+            inorden(n.getDerecha());
+        }
+    }
+   
+    public void inorden() {
+        this.inorden(this.raiz);
+    }
+
+    
+}
+
